9f7d51f2ab4e4fe3390c947b427e13400d80f85d
glhkang <glhkang@gmail.com>
2020-06-16 19:21:29 -0400

    deploying to heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -17,17 +18,15 @@ mongoose
 app.get("/", (req, res) => res.send("Hello World"));
 
 
-
-
 app.get("/logout", function (req, res) {
   req.logout();
   res.redirect("/");
 });
 
 
-
 app.use(passport.initialize());
 require("./config/passport")(passport);
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -37,3 +36,11 @@ app.use("/api/posts", posts)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
