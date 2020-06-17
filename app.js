ad7e7e8900988b1a738156a6226046fad478c4ff
glhkang <glhkang@gmail.com>
2020-06-17 15:34:58 -0400

    updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
+
 // app.get("/", (req, res) => res.send("Hello World"));
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
@@ -47,7 +49,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-//below for heroku
+// below for heroku
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
