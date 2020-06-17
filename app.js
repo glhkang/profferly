de27ba721bd71f632b2f4d388490fa68dd748eb9
glhkang <glhkang@gmail.com>
2020-06-16 21:41:21 -0400

    deploying to heroku

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ mongoose
 
 // app.get("/", (req, res) => res.send("Hello World"));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/src/index.js", "index.js"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-app.use("/", express.static(path.join(__dirname, "/frontend/src/index.js")));
+app.use("/", express.static(path.join(__dirname, "/client/build")));
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
