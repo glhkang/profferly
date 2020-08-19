8aa337061bc68b30bc8a0d39ba496902bff486fc
glhkang <glhkang@gmail.com>
2020-08-19 19:14:57 -0400

    refractoring for heroku

--- app.js
+++ app.js
@@ -100,7 +100,9 @@ app.use("/", express.static(path.join(__dirname, "./client/build")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/public"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "build", "public", "index.html")
+    );
   });
 }
 
